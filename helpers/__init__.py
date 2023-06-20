497e5dc1477bebaff02f4b52c77e87db349a2642
Marcello Vanzulli <marcello.vanzulli@gmail.com>
2023-06-20 23:24:58 +0200

    fix helpers mancanti

---
+++ helpers/__init__.py
@@ -0,0 +1,9 @@
+#
+# QualiCtrl è un progetto di Qualitade S.r.l.
+# (c) 2019 e anni successivi Qualitade S.r.l. - Ogni diritto è riservato.
+# (c) 2019 and following years by Qualitade S.r.l. -  All right reserved.
+#
+# Autore: Nicholas Fiorentini (nicholas.fiorentini@qualitade.com)
+#
+
+"""Helpers per i Notebook Jupyer."""
--- helpers/src/preprocessing.py
+++ helpers/src/preprocessing.py
@@ -1,36 +1,87 @@
 import numpy as np
 import pandas as pd
 from scipy.signal import savgol_filter
+from sklearn.preprocessing import FunctionTransformer
+from numpy import ndarray
 
-def msc(data):
-    # Convert DataFrame to numpy array
-    spectra = data.to_numpy()
-    # Subtract mean spectrum from each spectrum in the data
-    spectra -=  np.mean(spectra, axis=0)
-    # Divide each spectrum in the data by the standard deviation spectrum
-    spectra /= np.std(spectra, axis=0)
-    # Convert back to DataFrame
-    corrected_data = pd.DataFrame(spectra, columns=data.columns)
+def _msc(data):
+    
+    try:
+        # Convert DataFrame to numpy array
+        spectra = data.to_numpy()
+    except:
+        spectra = data.copy()
+        
+    # Mean and std spectrum
+    mean_spectrum = np.mean(spectra, axis=1, keepdims=True)
+    std_spectrum = np.std(spectra, axis=1, keepdims=True)
 
-    return corrected_data
+    # Apply MSC correction
+    msc_data = (spectra - mean_spectrum) / std_spectrum
 
-
-def smoothing(data, window_length=5, polyorder=2):
-    smoothed_data = pd.DataFrame(columns=data.columns)
-    for column in data.columns:
-        smoothed_data[column] = savgol_filter(data[column], window_length, polyorder)
-    return smoothed_data
-
-
-def derivative(data, deriv_order=1):
-    derivative_data = pd.DataFrame(columns=data.columns)
-    for column in data.columns:
-        derivative_spectrum = np.gradient(data[column], deriv_order)
-        derivative_data[column] = derivative_spectrum
-    return derivative_data
+    return msc_data
 
 
-def normalization(data):
-    # Column-wise standardization
-    normalized_df=(df-df.mean())/df.std()
-    return normalized_df
+def msc_processing() -> FunctionTransformer:
+    """Build the FunctionTranformer for the Pipeline."""
+    return FunctionTransformer(_msc, validate=True)
+
+
+# DERIVATE
+def _derivate(
+    values: ndarray, window_length: int = 7, polyorder: int = 2, derivate_order: int = 1
+) -> ndarray:
+    """
+    Derivate of a spectral data using
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.savgol_filter.html
+
+    This function always returns a numpy array.
+
+    values
+        The data to be filtered as an array-like object (numpy). If x is not a single or double precision floating
+        point array, it will be converted to type numpy.float64 before filtering.
+
+    window_length
+        The length of the filter window (i.e., the number of coefficients). window_length must be a positive odd
+        integer.
+        Default: 7
+
+    polyorder
+        The order of the polynomial used to fit the samples. polyorder must be less than window_length.
+        Default: 2
+
+    derivate_order
+        The order of the derivative to compute. This must be a nonnegative integer.
+        Default: 1  (first degree derivate)
+    """
+    return savgol_filter(
+        values, window_length=window_length, polyorder=polyorder, deriv=derivate_order
+    )
+
+
+def derivate_processing(
+    *, window_length: int = 7, polyorder: int = 2, derivate_order: int = 1
+) -> FunctionTransformer:
+    """Build the FunctionTranformer for the Pipeline."""
+    return FunctionTransformer(
+        _derivate,
+        validate=True,
+        kw_args={
+            "window_length": window_length,
+            "polyorder": polyorder,
+            "derivate_order": derivate_order,
+        },
+    )
+
+
+# NORMALIZZAZIONE
+def _norm(data):
+    mean = data.mean(axis=1, keepdims=True)
+    std = data.std(axis=1, keepdims=True)
+    standard_data = (pd.DataFrame(data) - mean ) / std
+    
+    return standard_data.values
+
+def norm_preprocessing() -> FunctionTransformer:
+    """Build the FunctionTranformer for the Pipeline."""
+    return FunctionTransformer(_norm, validate=True)
